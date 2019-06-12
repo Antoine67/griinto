4c10bae73fbb692b0750ab1b6b9542a86c8cb7b5
Antoine MOHR <antoine.mohr@viacesi.fr>
2019-06-12 15:37:14 +0200

    FTP folders

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,30 +1,51 @@
+"use strict";
 const electron = require('electron');
 const remote = electron.remote;
 const win = remote.getCurrentWindow();
 const { ipcRenderer } = electron;
 
+let appData = null ;
 
 	
 $( document ).ready(function() {
+
+	createAppData("index");
 	
 	$( "#app-close" ).click(function() { closeWindow() });
 
 	$( ".tab-button" ).click(function() {
 		let el = $( this );
+
+		//Visual effect
 		$( ".tab-button" ).removeClass("active");
 		el.addClass("active");
 
+		let tab = el.attr("data-tab");
+		if(tab.localeCompare(appData["currentTab"]) == 0) return; //Requested tab is already displayed
+
 		let callback = null;
 		//Which callback call ?
-		switch(el.attr("data-tab")) {
+		switch(tab) {
 			case "ftp" :
 				callback = bindFTPContent();
 				break;
 		}
 
-		//Clear content and display new one, and call callback when successfully displayed
-		$("#content-displayer").empty();
-		$("#content-displayer").load("assets/views/" + el.attr("data-tab") + ".html", callback);
+
+		if(appData[tab] != null) {
+		//There tab has already been created, we only get back the data and display it
+			$("#content-displayer").html(appData[tab]).promise().done(callback);
+			appData["currentTab"] = tab;
+		} else {
+		//There this tab has never been created, so we need to do it
+
+			//Clear content and display new one, and call callback when successfully displayed
+			saveAndClearContentDisplayer(tab);
+			$("#content-displayer").load("assets/views/" + el.attr("data-tab") + ".html", callback);
+		}
+
+
+		
 		
 	});
 
@@ -36,3 +57,16 @@ $( document ).ready(function() {
 
 function closeWindow() { win.close(); }
 
+function createAppData(initialTab) {
+	appData = new Object();
+	appData["currentTab"] = initialTab;
+
+	console.log(appData);
+}
+
+function saveAndClearContentDisplayer (newTab) {
+	let el = $("#content-displayer");
+	appData[appData["currentTab"]] = el.html(); //Save data into appData
+	appData["currentTab"] = newTab;
+	el.empty(); //Clear content
+}
--- app/assets/js/ftpClient.js
+++ app/assets/js/ftpClient.js
@@ -1,11 +1,28 @@
+"use strict";
+
 let jsftp = require("jsftp"); /* https://github.com/sergi/jsftp */
 let net = require("net");
 let fs = require("fs");
-var TreeView = require('js-treeview');
 
 var ftpConnection;
 let connected = false;
-let currentFolder = "/";
+let currentLocalPath;
+let currentRemotePath = "/";
+
+
+/**
+ * Constants for message displaying and button state
+ */
+const ERROR = -1;
+const LOADING = 0;
+const SUCCESS = 1;
+const INITIAL = 2;
+
+const LOCAL = 0;
+const REMOTE = 1;
+
+const INITIAL_LOCAL_PATH = "D:/";
+const INITIAL_REMOTE_PATH = "/";
 
 /**
  * Call to bind HTLM elements functions on "ftp.html" page
@@ -16,7 +33,7 @@ function bindFTPContent() {
   $( document ).ready(function() {
 
     //Cancel submit and create jsftp object with passed data
-    $("#ftpLoginForm").submit(function(e){
+    $( '#ftpLoginForm' ).submit(function(e){
         e.preventDefault();
         createFTPConnection(
           $('#ftpServer').val(),
@@ -26,14 +43,39 @@ function bindFTPContent() {
         );
         $('#ftp-login-modal').modal('hide');
     });
+
+    //Disconnection button is hide by default, and changing cwd disabled
+    $( '#ftpDisconnectButton' ).click(function(e){ closeFTPConnection (ftpConnection) });
+
+    //Input update -> change current working directory
+    $( '#local-searchbar' ).change(function() { updatePath( $(this).val(), LOCAL) });
+    $( '#remote-searchbar' ).change(function() { updatePath( $(this).val(), REMOTE) });
+
+    //When tab is restored
+    if(!connected) { 
+      $( '#ftpDisconnectButton' ).hide();
+      $( '#local-searchbar' ).prop('disabled', true);
+      $( '#remote-searchbar' ).prop('disabled', true);
+    }
+    $('#message-displayer').css('opacity', 0);
+    bindFilesFolderActions();
+
+
+
   });
-  
 };
 
 /* ----- FTP connection ----- */
 
+/**
+ * Instance the var "ftpConnection". If successfully connected, try to auth the user.
+ * @param {String} host Host name, or IP adress of a remote FTP server
+ * @param {Number} port Port (Usually 21)
+ * @param {String} user Username
+ * @param {String} pass Password
+ */
 function createFTPConnection(host, port = 21, user, pass) {
-  setButtonState(0);
+  setButtonState(LOADING);
   $('#remote-files').empty();
   ftpConnection = new jsftp({
     host: host,
@@ -45,12 +87,22 @@ function createFTPConnection(host, port = 21, user, pass) {
     },
   });
 
-  ftpConnection.on('error', (err) => connectionFailed(err)); //Server not reachable
+  ftpConnection.on('error', (err) => connectionFailed(err,"Erreur lors de la connexion, vérifiez l'adresse du serveur")); //Server not reachable
   ftpConnection.on('connect', (err) => authentification(host, port, user, pass)); //Server reachable
   //ftpConnection.on('data', function(data){console.log(data);}) //Debug 
 
 };
 
+/**
+ * Try to auth a user to a remote host, if success call connectionSuccessful() else display an error
+ * @param {String} host Host name, or IP adress of a remote FTP server
+ * @param {Number} port Port (Usually 21)
+ * @param {String} user Username
+ * @param {String} pass Password
+ * 
+ * @see connectionSuccessful()
+ * @see displayMessage()
+ */
 function authentification(host, port, user, pass) {
   connected = false;
   console.log("Connection to "+host+":"+port+" as '"+user+"' with password : '"+pass+"'");
@@ -58,9 +110,8 @@ function authentification(host, port, user, pass) {
   ftpConnection.auth(user,pass, (err) => {
     if(err) {
       console.log(err);
-      errStr = err.toString();
-      var errorMsg = errStr.substr(0, errStr.indexOf('\n')); 
-      displayMessage(errorMsg, -1);
+      let errorMsg = err.toString().split("\n"); 
+      connectionFailed(err,errorMsg[0]);
     }else {
       connectionSuccessful();   
     }
@@ -68,54 +119,91 @@ function authentification(host, port, user, pass) {
   });
 }
 
-
+/**
+ * Close an existing FTP connection and display message
+ * @param {jsftp} ftpConnection 
+ * @see displayMessage()
+ */
 function closeFTPConnection(ftpConnection) {
   ftpConnection.raw("quit", (err, data) => {
     if (err) {
-      return console.error(err);
+      console.error(err);
+      displayMessage(err, ERROR);
+      return ;
     }
-  
-    displayMessage("Déconnecté !",1);
+
+    clearFiles();
+    connected = false;
+
+    displayMessage("Déconnecté !", SUCCESS);
+    setButtonState(INITIAL);
+    $( '#ftpDisconnectButton' ).hide();
+    $( '#local-searchbar' ).prop('disabled', true).val("").attr("placeholder", "Chemin vers...");
+    $( '#remote-searchbar' ).prop('disabled', true).val("").attr("placeholder", "Chemin vers...");
+
   });
 };
 
-
+/**
+ * Call when authentification succeed, display success message and fill files
+ * 
+ * @see displayMessage()
+ */
 function connectionSuccessful ()  {
   connected = true;
-  setButtonState(1);
-  displayMessage("Connexion réussie !",1);
+
+  setButtonState(SUCCESS);
+  displayMessage("Connexion réussie !", SUCCESS);
   $("#connected-to").html("Connecté à <b>"+ftpConnection.host+"</b>");
-  fillLocalFiles("/");
-  fillRemoteFiles("/");
+
+  
+
+  //Allow disconnection and update on cwd
+  $( '#ftpDisconnectButton' ).show();
+  $( '#local-searchbar' ).prop('disabled', false);
+  $( '#remote-searchbar' ).prop('disabled', false);
+
+  //Fill view
+  updatePath(INITIAL_LOCAL_PATH, LOCAL);
+  updatePath(INITIAL_REMOTE_PATH, REMOTE);
+
+  
 
 };
 
-
-function connectionFailed (err) {
+/**
+ * Called while issuing trouble connecting to remote server
+ * @param {String} err Error message
+ */
+function connectionFailed (err, msgToDisplay) {
   console.log(err);
-  setButtonState(-1);
-  displayMessage("Erreur lors de la connexion",-1);
+  setButtonState(ERROR);
+  displayMessage(msgToDisplay, ERROR);
 }
 
-
-function isUserConnected () {
-  return connected;
-};
+/** User's connection state to server */
+function isUserConnected () {return connected;};
 
 
 /* ----- Display ----- */
-function displayMessage (msg, code=0) {  
+
+/**
+ * Display message on view
+ * @param {String} msg Message to display
+ * @param {Int} code Type of message (ERROR, SUCCESS or LOADING)
+ */
+function displayMessage (msg, code = SUCCESS) {  
   let el = $('#message-displayer');
   el.html(msg);
   el.removeClass("alert-success"); el.removeClass("alert-primary"); el.removeClass("alert-danger"); 
   switch (code) {
-    case -1:
+    case ERROR:
       el.addClass("alert-danger"); 
       break;
-    case 0:
+    case LOADING:
       el.addClass("alert-primary"); 
       break;
-    case 1:
+    case SUCCESS:
       el.addClass("alert-success"); 
       break;
   }
@@ -126,22 +214,34 @@ function displayMessage (msg, code=0) {
 
 }
 
-function setButtonState (state = -1) {
+/**
+ * Change connection button's state
+ * @param {Number} state State of button (ERROR, SUCCESS, LOADING or INITIAL)
+ */
+function setButtonState (state = ERROR) {
   let el = $("#ftpConnectionState");
-
   el.removeClass("btn-primary"); el.removeClass("btn-danger"); el.removeClass("btn-success");  el.removeClass("btn-info");
+
   switch (state) {
-    case -1 :
+
+    case ERROR :
       el.html('<i class="fas fa-times"></i>&nbsp;Non connecté');
       el.addClass("btn-danger");
       break;
-    case 0:
+
+    case LOADING:
       el.html('<i class="fas fa-spinner"></i>&nbsp;Connexion...');
       el.addClass("btn-info");
       break;
-    case 1 :
+
+    case SUCCESS :
       el.html('<i class="fas fa-check-circle"></i>&nbsp;Connecté');
       el.addClass("btn-success");
+      break;
+
+    case INITIAL :
+      el.html('Se connecter');
+      el.addClass("btn-primary");
       break;
   }
 }
@@ -149,90 +249,25 @@ function setButtonState (state = -1) {
 
 /* ----- FTP functions on files / folder ----- */
 
-function listFiles(ftpConnection, remoteCWD) {
-  if (!isUserConnected()) {
-    userShouldConnect();
-    return;
-  }
-  ftpConnection.list(remoteCWD, (err, res) => {
-    return res;
-  });
+/**
+ * 
+ * @param {jsftp} ftpConnection 
+ * @param {String} remoteCWD 
+ * @param {*} callback Function to pass data when done
+ * @return an Array of files
+ */
+function listFiles(ftpConnection, remoteCWD, callback) {
+  if (!isUserConnected()) { return; }
+  ftpConnection.list(remoteCWD, (err, res) => { callback(res); });
 }
 
 
-function fillRemoteFiles(cwd,ftp = ftpConnection) {
-
-  let filesAndFolders = [];
-  ftp.ls(cwd, (err, res) => {
-    console.log(res);
-    res.forEach(function(fileOrFolder) {
-      let type = fileOrFolder.type==1 ? 'folder' : 'file';
-      filesAndFolders.push({ name: fileOrFolder.name , type : type});
-    });
-    createFolderFileView('remote-files',filesAndFolders);
-  });
-
-}
-
-
-
-
-
-/* ----- Local folder functions ----- */
-
-
-function fillLocalFiles(cwd) {
-  
-  let filesAndFolders = [];
-  getAllFilesFromFolder(cwd).forEach(function(fileOrFolder) {
-    filesAndFolders.push({ name: fileOrFolder, type: 'file' })
-  });
-
-  //var tree = new TreeView(filesAndFolders, 'local-files');
-  createFolderFileView('local-files',filesAndFolders);
-
-}
-
-function createFolderFileView(divId, dataArr) {
-  let el = $('#'+divId); el.empty();
-
-  dataArr.forEach(function(fileOrFolder) {
-    let logoClass = !fileOrFolder.type.localeCompare('folder') ? 'far fa-folder' : 'far fa-file';
-    el.append("<div class='folderFile'><i class='folderFileLogo "+logoClass+"'></i><div class='folderFileName'>"+fileOrFolder.name+"</div></div>");
-  });
-
-}
-
-
-
-
-function getAllFilesFromFolder (dir) {
-
-  let results = [];
-
-  fs.readdirSync(dir).forEach(function(file) {
-
-      path = dir+" "+file;
-      //var stat = fs.lstatSync(path)
-      /*
-      if (stat && stat.isDirectory()) {
-          results = results.concat(getAllFilesFromFolder(file))
-      } else results.push(file);
-      */
-     results.push(file);
-  });
-
-  return results;
-
-};
-
-
-
-
-
-
-
-
+/**
+ * Create folder on remote server
+ * @param {jsftp} ftp FTP connection
+ * @param {String} cwd Path
+ * @param {String} name Folder's name
+ */
 function createFolder(ftp, cwd, name) {
   ftp.raw("mkd", cwd+name, (err, data) => {
     if (err) {
@@ -244,4 +279,205 @@ function createFolder(ftp, cwd, name) {
 }
 
 
+/**
+ * Fill view with files from remote server
+ * @param {String} cwd Path where to find files
+ * @param {jsftp} ftp FTP connection
+ * 
+ * @see createFolderFileView()
+ */
+function fillRemoteFiles(cwd,ftp = ftpConnection) {
 
+  let filesAndFolders = [];
+  ftp.ls(cwd, (err, res) => {
+    if(err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(res);
+    res.forEach(function(fileOrFolder) {
+      let type = fileOrFolder.type==1 ? 'folder' : 'file';
+      filesAndFolders.push({ name: fileOrFolder.name , type : type, path : cwd+"/"+fileOrFolder.name });
+    });
+    createFolderFileView(REMOTE, filesAndFolders, cwd);
+    currentRemotePath = cwd;
+  });
+}
+
+
+
+/* ----- Local folder functions ----- */
+
+/**
+ * Fill view with local files
+ * @param {String} cwd Path where to find files
+ * 
+ * @see createFolderFileView()
+ */
+function fillLocalFiles(cwd) {
+  getAllFilesFromFolder(cwd, (files) => { createFolderFileView(LOCAL, files, cwd); currentLocalPath = cwd; });
+}
+
+/**
+ * 
+ * @param {Number} type Local or remote server ?
+ * @param {Array<Object>} dataArr Array filled with object containing data from files/folders
+ */
+function createFolderFileView(type, dataArr, cwd) {
+  let el;
+  //LOCAL
+  if(type == LOCAL) {
+    el = $('#local-files'); el.empty();
+    dataArr.forEach(function(fileOrFolder) {
+      let logoClass;  let primaryClass;
+      if( !fileOrFolder.type.localeCompare('folder') ) {
+        logoClass = 'far fa-folder';
+        primaryClass = 'folderFile folder-local';
+      }else {
+        logoClass = 'far fa-file';
+        primaryClass = 'folderFile file-local';
+      }
+      el.append("<div class='"+primaryClass+"' data-path='"+fileOrFolder.path+"'><i class='folderFileLogo "+logoClass+"'></i><div class='folderFileName'>"+fileOrFolder.name+"</div></div>");
+    });
+
+  //REMOTE
+  }else if (type == REMOTE) {
+    el = $('#remote-files');  el.empty();
+    dataArr.forEach(function(fileOrFolder) {
+      let logoClass;  let primaryClass;
+      if( !fileOrFolder.type.localeCompare('folder') ) {
+        logoClass = 'far fa-folder';
+        primaryClass = 'folderFile folder-remote';
+      }else {
+        logoClass = 'far fa-file';
+        primaryClass = 'folderFile file-remote';
+      }
+      el.append("<div class='"+primaryClass+"' data-path='"+fileOrFolder.path+"'><i class='folderFileLogo "+logoClass+"'></i><div class='folderFileName'>"+fileOrFolder.name+"</div></div>");
+    });
+  }else return console.error("No type specified");
+  
+  //Parent folder
+  let parentType = type==LOCAL?"folderFile folder-local":"folderFile folder-remote";
+  let parentPath = getParentFolder(cwd);
+  el.prepend("<div class='"+parentType+"' data-path='"+parentPath+"'><i class='folderFileLogo far fa-folder'></i><div class='folderFileName'>..</div></div>")
+
+  //Need to bind our new classes
+  bindFolderClick();
+
+
+}
+
+
+//TODO full async
+/**
+ * 
+ * @param {String} cwd Local path where to find files
+ * @param {*} callback Function to pass data when done 
+ * @returns an Array of Object fullfilled with data from folders and files
+ */
+async function getAllFilesFromFolder (cwd,callback) {
+
+  let results = [];
+
+  fs.readdir(cwd, (err, list) => {
+    if(err) {
+      console.error(err);
+      updatePath(currentLocalPath, LOCAL);
+      return;
+    };
+    
+    list.sort();
+    list.forEach(function(file) {
+      let path = cwd+"/"+file;
+      
+      try {
+
+        let stat = fs.statSync(path); let type = "file";
+
+        if(stat.isDirectory()) {type = 'folder';}
+        else type = 'file';
+        results.push({ name: file, type: type, path: path })
+
+      }catch(e) {/*console.log(e);*/ /* No permission on file/folder */}
+
+    });
+    callback(results);
+  });
+};
+
+/* ----- View update ----- */
+
+
+/**
+ * Remove all files from view
+ */
+function clearFiles() {
+  $( '#remote-files' ).empty();
+  $( '#local-files' ).empty();
+}
+
+/**
+ * Update local or remote current path, and fill data into corresponding area
+ * @param {String} cwd 
+ * @param {Number} type 
+ */
+function updatePath(cwd, type) {
+  if(! (cwd && cwd.localeCompare("") != 0)) cwd = "/"; // cwd empty or null
+  cwd = formateCwd(cwd);
+  console.log("Update path "+cwd);
+  let el;
+  if(type == LOCAL) {
+    el = $( '#local-searchbar' );
+    fillLocalFiles(cwd); // Current path will only be updated when data has filled
+  }else if(type == REMOTE) {
+    el = $( '#remote-searchbar' );
+    fillRemoteFiles(cwd); // Current path will only be updated when data has filled
+  }else return console.error("Update path should be either on LOCAL or REMOTE");
+
+  el.attr("placeholder", cwd);
+  el.val(cwd);
+  
+}
+
+/**
+ * Formate current working directory, removing double backslash and incorrect form
+ * @param {String} str  
+ * @return formattedCwd string
+ */
+function formateCwd(str) {
+  if(str.slice(-1) == ':') return str + "/";
+
+  while(str.includes("//") || str.includes("\\\\")) {
+    str = str.replace("//", "/");
+    str = str.replace("\\\\", "/");
+  }
+  return str;
+}
+
+/**
+ * Get the parent folder of another folder
+ * @param {String} pathToFolder 
+ * @returns {String} the parent path
+ */
+function getParentFolder(pathToFolder) {
+  if(pathToFolder.slice(-1) == '/' || pathToFolder.slice(-1) == '\\') pathToFolder =  pathToFolder.slice(0,-1); // End with a slash
+  pathToFolder = formateCwd(pathToFolder); // Remove multi slash
+  let newWd = "";
+  let splited = pathToFolder.split("/");
+  for(let i= 0; i < splited.length-1; i++) { newWd += splited[i] + "/"; }
+
+  return newWd;
+}
+
+
+function bindFolderClick() {
+  //Open folder on click
+  $( '.folder-local' ).click(function() { updatePath( $(this).data("path"), LOCAL) });
+  $( '.folder-remote' ).click(function() { updatePath( $(this).data("path"), REMOTE) });
+}
+
+function bindFilesFolderActions() {
+  bindFolderClick();
+  
+}
